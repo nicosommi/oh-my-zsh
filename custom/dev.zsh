ffe8579c05807813a9bc05b83dbfa34ce4fe6913
Nico Sommi <nicosommi@gmail.com>
2019-03-29 19:14:49 -0300

    Adds vars for workspace extension

--- custom/dev.zsh
+++ custom/dev.zsh
@@ -1,11 +1,13 @@
 workspace=~/workspace
 ide=code
+ide_ws_extension=code-workspace
+text_editor=code
 
 alias ws="cd $workspace"
 
 remember-passphrase-per-session() {
   # TODO: copy to clipboard for macos and linux instead, but don't automate
-  echo "Add this at the first line in with the editor: AddKeysToAgent yes"
-  read -s -k '?Press any key to continue.'
-  $ide ~/.ssh/config
+  echo "Add this at the first line in ~/.ssh/config with a text editor: AddKeysToAgent yes"
+  read -s -k '?Press any key to open it with the text editor or CTRL+C to abort.'
+  $text_editor ~/.ssh/config
 }
