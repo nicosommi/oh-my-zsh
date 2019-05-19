5d7b86d08df2f332e454485c77cbd2420987eb3d
Nico Sommi <nicosommi@gmail.com>
2019-05-19 17:31:01 -0300

    Adds brave code with theia basic

--- custom/dev.zsh
+++ custom/dev.zsh
@@ -2,6 +2,8 @@ workspace=~/workspace
 ide=code
 ide_ws_extension=code-workspace
 text_editor=code
+brave=/Applications/Brave.app/Contents/MacOS/Brave\ Browser
+firefox=/Applications/Firefox.app/Contents/MacOS/firefox
 
 alias ws="cd $workspace"
 
@@ -10,4 +12,20 @@ remember-passphrase-per-session() {
   echo "Add this at the first line in ~/.ssh/config with a text editor (copied to clipboard): AddKeysToAgent yes"
   read -s -k '?Press any key to open it with the text editor or CTRL+C to abort.'
   $text_editor ~/.ssh/config
+}
+
+brave-pwa() {
+  $brave --app=$1
+}
+
+code-pwa() {
+  local wd=${$(pwd)//\//\_}
+  pushd $1
+  docker run -d $2 -p 23000:3000 -v "$(pwd):/home/project:cached" --name "code-$wd" theiaide/theia:next
+  brave-pwa http://localhost:23000
+  popd
+}
+
+code-pwa-rm() {
+  code-pwa $1 --rm
 }
